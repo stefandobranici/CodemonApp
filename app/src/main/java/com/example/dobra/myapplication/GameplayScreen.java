37b8a1ae0c7d30b81e2a34f536fee9d6d89f098e
Cosmin Stefan Dobranici <dobranici.cosmin@yahoo.com>
2019-01-29 18:06:57 +0000

    Third commit

--- app/src/main/java/com/example/dobra/myapplication/GameplayScreen.java
+++ app/src/main/java/com/example/dobra/myapplication/GameplayScreen.java
@@ -3,27 +3,46 @@ package com.example.dobra.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class GameplayScreen extends AppCompatActivity {
 
-    private RelativeLayout gamescreen;
+    private RelativeLayout topscreen, middlescreen;
+    private float scale;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gameplay_screen);
 
-        gamescreen = (RelativeLayout) findViewById(R.id.gamescreenlayout);
+        topscreen = (RelativeLayout) findViewById(R.id.topscreenlayout);
+        middlescreen = (RelativeLayout) findViewById(R.id.middlescreenlayout);
+
+        scale = getResources().getDisplayMetrics().density;
 
         if(LevelAdapter.CURRENT_GAME_MISSION.colour_label == 2){
             ImageView image = new ImageView(this);
             image.setImageResource(R.drawable.trainer);
 
-            image.setX(300);
-            image.setY(300);
+            int dpWidthInPx = (int) (360 * scale);
+            int dpHeightInPx = (int) (180 * scale);
 
-            gamescreen.addView(image);
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(dpWidthInPx, dpHeightInPx);
+            image.setLayoutParams(layoutParams);
+
+            topscreen.addView(image);
+
+            TextView text = new TextView(this);
+
+            text.setText("Hello! How are you doing!");
+            text.setX(middlescreen.getX());
+            text.setY(middlescreen.getY());
+
+            middlescreen.addView(text);
+
+
         }
     }
 }
