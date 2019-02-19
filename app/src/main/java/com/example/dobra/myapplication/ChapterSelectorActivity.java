05dc0f74f24c8abfc03247679095e37666ebb830
Cosmin Stefan Dobranici <dobranici.cosmin@yahoo.com>
2019-02-19 00:39:25 +0000

    Added more database functionality

--- app/src/main/java/com/example/dobra/myapplication/ChapterSelectorActivity.java
+++ app/src/main/java/com/example/dobra/myapplication/ChapterSelectorActivity.java
@@ -7,13 +7,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 public class ChapterSelectorActivity extends AppCompatActivity {
 
@@ -21,10 +26,61 @@ public class ChapterSelectorActivity extends AppCompatActivity {
 
     public static Integer chapter_selected;
 
+    private FirebaseAuth mAuth;
+
+    private FirebaseDatabase mDatabase;
+
+    private DatabaseReference mChaptersReference;
+
+    private DatabaseReference mLevelReference;
+
+    private List<Integer> chapterProgress;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story_chapter_selector);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mDatabase = FirebaseDatabase.getInstance();
+
+        String currentUser = mAuth.getCurrentUser().getUid();
+
+        chapterProgress = new ArrayList<>();
+        for(int i = 0 ; i < 1; i++){
+            chapterProgress.add(0);
+        }
+
+        for(int i = 1 ; i < 2; i++) {
+            Integer currentChapter = i;
+            mChaptersReference = mDatabase.getReference("Users").child(currentUser).child("Chapters").child(currentChapter.toString());
+
+            mChaptersReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if(dataSnapshot.exists()){
+                        for(DataSnapshot keyNode : dataSnapshot.getChildren()){
+                            Boolean completed = keyNode.getValue(Boolean.class);
+                            if(completed) chapterProgress.set(0, chapterProgress.get(0)+1);
+                        }
+                    } else {
+                        //Map<Integer, Boolean> newPost = new HashMap();
+                        for(int i = 1; i < 22; i++){
+                            Integer level = i;
+                            DatabaseReference currentLevel = mChaptersReference.child(level.toString());
+                            currentLevel.setValue(false);
+                        }
+                        //mChaptersReference.setValue(newPost);
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+        }
 
         variablesButtonOnClickListener();
         ifButtonOnClickListener();
--- app/src/main/java/com/example/dobra/myapplication/ModeSelectorActivity.java
+++ app/src/main/java/com/example/dobra/myapplication/ModeSelectorActivity.java
@@ -5,14 +5,29 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.IBinder;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.ImageButton;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ModeSelectorActivity extends AppCompatActivity {
 
     private ImageButton binauralbeatsBtn, mnemonicsBtn, plainBtn;
+
+    private FirebaseDatabase mDatabase;
+
+    private FirebaseAuth mAuth;
+
+    private DatabaseReference modeSelectorReference;
 
     HomeWatcher mHomeWatcher;
 
@@ -21,6 +36,35 @@ public class ModeSelectorActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mode_selector);
+
+        mDatabase = FirebaseDatabase.getInstance();
+
+        mAuth = FirebaseAuth.getInstance();
+
+        String currentUser = mAuth.getCurrentUser().getUid();
+
+        modeSelectorReference = mDatabase.getReference("Users").child(currentUser).child("mode");
+
+        modeSelectorReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String modeSelected = dataSnapshot.getValue(String.class);
+
+
+                if(!TextUtils.isEmpty(modeSelected)){
+                    Intent gamemode_intent = new Intent("android.intent.action.ChapterSelectorActivity");
+                    startActivity(gamemode_intent);
+                    finish();
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
 
         binauralBeatsButtonOnClickListener();
         mnemonicsButtonOnClickListener();
@@ -55,6 +99,10 @@ public class ModeSelectorActivity extends AppCompatActivity {
         binauralbeatsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String mode = "binaural";
+
+                modeSelectorReference.setValue(mode);
+
                 Intent gamemode_intent = new Intent("android.intent.action.ChapterSelectorActivity");
                 startActivity(gamemode_intent);
                 finish();
