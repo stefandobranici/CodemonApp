bb71c8f3b67a1c9e07dd2f2187735e3cd9a9132a
Cosmin Stefan Dobranici <dobranici.cosmin@yahoo.com>
2019-02-21 23:34:39 +0000

    Added more database functionality

--- app/src/main/java/com/example/dobra/myapplication/GameplayScreen.java
+++ app/src/main/java/com/example/dobra/myapplication/GameplayScreen.java
@@ -8,19 +8,51 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class GameplayScreen extends AppCompatActivity {
+    private Level level;
+
+    private LinearLayout topLayout, firstHalfTopLayout, secondHalfTopLayout, middleLayout, bottomLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gameplay_screen);
+
+        topLayout = (LinearLayout) findViewById(R.id.topScreenLayout);
+
+        firstHalfTopLayout = (LinearLayout) findViewById(R.id.topScreenLayoutFirstHalf);
+
+        secondHalfTopLayout = (LinearLayout) findViewById(R.id.topScreenLayoutSecondHalf);
+
+        middleLayout = (LinearLayout) findViewById(R.id.middleScreenLayout);
+
+        bottomLayout = (LinearLayout) findViewById(R.id.bottomScreenLayout);
+
+        level = CurrentUserInformation.getInstance().getLevelSelectedForPlay();
+
+        if(level.isGreen()){
+            drawScreenLearning();
+        } else if(level.isViolette()) {
+            drawScreenTraining();
+        } else if(level.isRed()) {
+            drawScreenBoss();
+        }
+
+
     }
 
     private void drawScreenLearning(){
+        ImageView trainer = new ImageView(this);
 
+        trainer.setImageResource(R.drawable.trainer);
+
+        topLayout.addView(trainer);
     }
 
     private void drawScreenTraining(){
+        ImageView chiuplus = new ImageView(this);
 
+        chiuplus.setImageResource(R.drawable.chiuplusback);
     }
 
     private void drawScreenBoss(){
